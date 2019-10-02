aa03031b36ccfe70063c86792e56a4cd3664f555
Acerizm <34855234+Acerizm@users.noreply.github.com>
2019-10-03 06:12:16 +0800

    Sidebar feature has been implemented

--- perfectbride/src/Components/Navbar/index.js
+++ perfectbride/src/Components/Navbar/index.js
@@ -39,30 +39,63 @@ const HamburgerIcon = withStyles({
   }
 })(MenuIcon);
 
+const SideNavbar = (props) => {
+  let currentSideBarStatus;
+  console.log(props.sideNavbarStatus);
+  if (props.sideNavbarStatus == null){
+    currentSideBarStatus = false;
+  }
+  else if (props.sideNavbarStatus == true){
+    currentSideBarStatus = true;
+  }
+  else if (props.sideNavbarStatus == false){
+    currentSideBarStatus = false;
+  }
+  return(
+    <div id="mySidenav" className="sidenav" style={{
+      // height: "100%",
+      // position: "fixed",
+      // zIndex: "1",
+      // top: 0,
+      // left: 0,
+      // backgroundColor: "#111",
+      // overflowX: "hidden",
+      // transition: "0.5s",
+      // paddingTop: "60px",
+      width: currentSideBarStatus ? "250px" : "0px"}}>
+      <a className="closebtn" onClick={() => props.changeSidebarStatus()}>&times;</a>
+      <Link to={ROUTES.HOME_PAGE} className="link"><span className="NavLink">Home</span></Link>
+      <Link to={ROUTES.ORGANIZER_PAGE} className="link"><span className="NavLink">Organizer</span></Link>
+      <Link to={ROUTES.SPONSORS_PAGE} className="link"><span className="NavLink">Sponsors</span></Link>
+      <Link to={ROUTES.GALLERY_PAGE} className="link"><span className="NavLink">Gallery</span></Link>
+      <Link to={ROUTES.HOME_PAGE} className="link"><span className="NavLink">Videos</span></Link>
+    </div>
+  )
+}
+
 const TabletNavbar = () => {
    // animation is based on https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sidenav
 
   const [openSideNavbar, setSideNavbarStatus] = useState(false);
-  let SideNavbar;
+  let currentSideNavbar;
   //code will only run if called
   const changeSidebarStatus = () => {
     if (openSideNavbar === true){
       setSideNavbarStatus(false);
-      SideNavbar = <SideNavbar sideNavbarStatus={openSideNavbar}/>
+      currentSideNavbar = <SideNavbar sideNavbarStatus={openSideNavbar} changeSidebarStatus={changeSidebarStatus}/>
     }
     else if (openSideNavbar === false){
       setSideNavbarStatus(true);
-      SideNavbar = <SideNavbar sideNavbarStatus={false}/>
-      debugger;
+      currentSideNavbar = <SideNavbar sideNavbarStatus={openSideNavbar} changeSidebarStatus={changeSidebarStatus}/>
     }
   }
 
   let status = openSideNavbar;
   //code will only run for the first time
-  if (SideNavbar == null){
-    SideNavbar = <SideNavbar sideNavbarStatus={status}/>
+  if (currentSideNavbar == null){
+    currentSideNavbar = <SideNavbar sideNavbarStatus={status} changeSidebarStatus={changeSidebarStatus}/>
   }
-  console.log(SideNavbar);
+  console.log(status);
   return(
     <div id="NavBarContainer">
       <div id="PerfectBrideHeadingContainer">
@@ -71,45 +104,11 @@ const TabletNavbar = () => {
       <div id="HamburgerMenu" onClick={() => changeSidebarStatus()}>
         <HamburgerIcon></HamburgerIcon>
       </div>
-      {SideNavbar}
+      {currentSideNavbar}
     </div>
     )
   }
 
-const SideNavbar = (props) => {
-  let currentSideBarStatus;
-  if (props.sideNavbarStatus == null){
-    currentSideBarStatus = false;
-    console.log(currentSideBarStatus);
-  }
-  else if (props.sideNavbarStatus == true){
-    currentSideBarStatus = true;
-  }
-  else if (props.sideNavbarStatus == false){
-    currentSideBarStatus = false;
-    console.log(currentSideBarStatus);
-  }
-  console.log(currentSideBarStatus);
-  return(
-    <div id="mySidenav" className="sidenav" style={{
-      height: "100%",
-      position: "fixed",
-      zIndex: "1",
-      top: 0,
-      left: 0,
-      backgroundColor: "#111",
-      overflowX: "hidden",
-      transition: "0.5s",
-      paddingTop: "60px",
-      width: currentSideBarStatus ? "250px" : "0px"}}>
-      <a className="closebtn">&times;</a>
-      <a>About</a>
-      <a>Services</a>
-      <a>Clients</a>
-      <a>Contact</a>
-    </div>
-  )
-}
   
 const MobileNavbar = () => {
   return (
