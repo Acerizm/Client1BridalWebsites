c90c5bc9d1ce4999a457673c4c8ee3009f5f7472
EC2 Default User <ec2-user@ip-172-31-24-123.ap-southeast-1.compute.internal>
2019-10-07 03:15:03 +0000

    Idk what I'm doing

--- perfectbride/src/Components/HomePage/index.js
+++ perfectbride/src/Components/HomePage/index.js
@@ -1,4 +1,4 @@
-import React,{Component} from "react";
+import React,{Component, useState} from "react";
 import "./index.css";
 import NavBar from "../Navbar/index.js";
 import * as TEXT from "./text.js";
@@ -49,6 +49,48 @@ const DesktopHomePage = (props) => {
 }
 
 const TabletHomePage = (props) => {
+  
+  const [currentImageClicked, setCurrentImageClickedIndex] = useState(0);
+  //let 0 be the event where the user does not hover the mouse on the images;
+  const [Image1Status,setImage1Status] = useState(false);
+  const [Image2Status,setImage2Status] = useState(false);
+  const [Image3Status,setImage3Status] = useState(false);
+  
+  const setImageStatus = (imageNum) => {
+    let currentStatus;
+    if (imageNum == 1){
+      if (Image1Status == false){
+        currentStatus = true
+      }
+      else{
+        currentStatus = false;
+      }
+      setImage1Status(currentStatus);
+    }
+    else if (imageNum == 2){
+      let currentStatus;
+      if (Image2Status == false){
+        currentStatus = true;
+      }
+      else{
+        currentStatus = false;
+      }
+      setImage2Status(currentStatus);
+    }
+    else if (imageNum == 3){
+      let currentStatus;
+      if (Image3Status == false){
+        currentStatus = true;
+      }
+      else{
+        currentStatus = false;
+      }
+      setImage3Status(currentStatus);
+    }
+  }
+  
+  
+  
   return (
     <div id="HomePage">
       <div id="FirstContentContainer">
@@ -60,7 +102,15 @@ const TabletHomePage = (props) => {
         </div>
       </div>
       <div id="OtherImagesArea">
-        <div id="OtherImages1"></div>
+        <div id="OtherImages1"
+          onClick={()=> setImageStatus(1)}
+          onMouseLeave={()=> setImageStatus(1)}
+          style={{
+            gridColumnStart: Image1Status ? "2" : "2",
+            gridColumnEnd: Image1Status ? "4" : "2",
+            width: Image1Status ? "100%" : "90%"
+          }}
+        ></div>
         <div id="OtherImages2"></div>
         <div id="OtherImages3"></div>
       </div>
