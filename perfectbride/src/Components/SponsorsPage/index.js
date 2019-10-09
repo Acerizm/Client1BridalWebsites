61b3d7deb2ee554062079dbb161d81aea3dc891e
Acerizm <34855234+Acerizm@users.noreply.github.com>
2019-10-09 22:28:29 +0800

    random update

--- perfectbride/src/Components/SponsorsPage/index.js
+++ perfectbride/src/Components/SponsorsPage/index.js
@@ -15,6 +15,12 @@ const SponsorsPage = (props) => {
   if (currentWindowWidth >= minDesktopWidth){
     currentSponsorPage = <DesktopSponsorPage/>
   }
+  else if (currentWindowWidth >= minTabletWidth && currentWindowWidth < minDesktopWidth){
+    currentSponsorPage = <TabletSponsorPage/>
+  }
+  else{
+    currentSponsorPage = "";
+  }
   return currentSponsorPage;
 }
 
@@ -49,35 +55,35 @@ const DesktopSponsorPage = (props) => {
   )
 }
 
-  const TabletSponsorPage = (props) => {
-    return (
-      <div id="SponsorsPageContainer">
-        <span id="SponsorTitle">{TEXT.SponsorTitle}</span>
-        <div id="Sponsor1">
-          <div className="SponsorImages" id="Sponsor1Image"></div>
-          <div id="SponsorTextArea">
-            <span className="SponsorsText" className="FirstSponsorText">{TEXT.Sponsor1Chi}</span>
-            <span className="SponsorsText" className="SecondSponsorText">{TEXT.Sponsor1Name}</span>
-            <span className="SponsorsText" className="ThirdSponsorText">{TEXT.Sponsor1Desc}</span>
-          </div>
-        </div>
-        <div id="Sponsor2">
-          <div id="SponsorTextArea">
-            <span className="SponsorsText" className="FirstSponsorText">{TEXT.Sponsor2Chi}</span>
-            <span className="SponsorsText" className="SecondSponsorText">{TEXT.Sponsor2Name}</span>
-            <span className="SponsorsText" className="ThirdSponsorText">{TEXT.Sponsor2Desc}</span>
-          </div>
-          <div className="SponsorImages" id="Sponsor2Image"></div>
-        </div>
-        <div id="CollectiveSponsorDesc">
-          <div id="CollectiveSponsorTextBox">
-            <div id="InnerCollectiveSponsorText">
-            {TEXT.CollectiveSponsorText}
-            </div>
-          </div>   
+const TabletSponsorPage = (props) => {
+  return (
+    <div id="SponsorsPageContainer">
+      <span id="SponsorTitle">{TEXT.SponsorTitle}</span>
+      <div id="Sponsor1">
+        <div className="SponsorImages" id="Sponsor1Image"></div>
+        <div id="SponsorTextArea">
+          <span className="SponsorsText" className="FirstSponsorText">{TEXT.Sponsor1Chi}</span>
+          <span className="SponsorsText" className="SecondSponsorText">{TEXT.Sponsor1Name}</span>
+          <span className="SponsorsText" className="ThirdSponsorText">{TEXT.Sponsor1Desc}</span>
         </div>
       </div>
-    )
-  }
+      <div id="Sponsor2">
+        <div id="SponsorTextArea">
+          <span className="SponsorsText" className="FirstSponsorText">{TEXT.Sponsor2Chi}</span>
+          <span className="SponsorsText" className="SecondSponsorText">{TEXT.Sponsor2Name}</span>
+          <span className="SponsorsText" className="ThirdSponsorText">{TEXT.Sponsor2Desc}</span>
+        </div>
+        <div className="SponsorImages" id="Sponsor2Image"></div>
+      </div>
+      <div id="CollectiveSponsorDesc">
+        <div id="CollectiveSponsorTextBox">
+          <div id="InnerCollectiveSponsorText">
+          {TEXT.CollectiveSponsorText}
+          </div>
+        </div>   
+      </div>
+    </div>
+  )
+}
 
 export default SponsorsPage;
